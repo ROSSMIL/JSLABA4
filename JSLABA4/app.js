c8d8542603ce786e54b937d3bace360ad2b4af72
ROSSMIL <112856396+ROSSMIL@users.noreply.github.com>
2022-10-19 19:30:09 +0300

    Add files via upload

---
+++ JSLABA4/app.js
@@ -0,0 +1,364 @@
+console.log("Завдання 1")
+var v = "Вірно";
+var n = "Невірно";
+
+let a=0; 
+let a1=1
+let a2=-3
+
+if(a==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(a1==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(a2==0){
+    console.log(v) 
+}
+else{
+    console.log(n)
+}
+   
+
+console.log('\n');
+
+console.log("Завдання 2")
+
+if(a>0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1>0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a2>0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+console.log("Завдання 3")
+
+if(a<0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1<0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a2<0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 4")
+
+if(a>=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1>=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a2>=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 5 ")
+
+if(a<=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1<=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a2<=0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 6")
+
+if(a!==0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1!==0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a2!==0){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 7")
+
+let atest="test", a1тест="тест", a23=3;
+
+if(atest=="test"){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a1тест=="test"){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a23=="test"){
+ console.log(v) 
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 8")
+
+let a8 = '1', a8a = 1, a8b = 3;
+if(a8 === '1' && a8 === '1'){
+console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a8a === '1' && a8a === '1'){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(a8b === '1' && a8b === '1'){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 9")
+
+let testT=true; testF=false 
+
+if(testT==true){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(testF==true){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 10")
+
+if(testT!=true){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(testF!=true){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 11")
+
+let a11=5; b11=0; c11=-3; d11=2
+
+if(a11>0 && a11<5){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(b11>0 && b11<5){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(c11>0 && c11<5){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+if(d11>0 && d11<5){
+ console.log(v)
+}
+else{
+ console.log(n)
+}
+
+console.log('\n');
+
+console.log("Завдання 12")
+
+let a12=5; b12=0; c12=-3; d12=2
+
+if(a12 == 0 || a12 == 2){
+ console.log(a12+7)
+}
+else{
+ console.log(a12/10)
+}
+if(b12 == 0 || b12 == 2){
+ console.log(b12+7)
+}
+else{
+ console.log(b12/10)
+}
+if(c12 == 0 || c12 == 2){
+ console.log(c12+7)
+}
+else{
+ console.log(c12/10)
+}
+if(d12 == 0 || d12 == 2){
+ console.log(d12+7)
+}
+else{
+ console.log(d12/10)
+}
+
+console.log('\n');
+
+console.log("Завдання 13")
+
+let  a13=1; b13=3; 
+let  a13a=0; b13b=6;
+let  a13b=3; b13c=5
+
+if(a13<=1 && b13>=3){
+ console.log(a13    +   b13)
+}
+else{
+ console.log(a13    -   b13)
+}
+if (a13a<=1 && b13b>=3){
+console.log  (a13a  +   b13b)
+}
+else{
+console.log  (a13a  -  b13b)
+}
+if(a13b<=1 && b13c>=3){
+console.log(a13b    +   b13c)
+}
+else{
+console.log(a13b    -   b13c)
+}
+
+console.log('\n');
+
+console.log("Завдання 14")
+
+let a14a=1; b14a=3;
+let a14b=0; b14b=6;
+let a14c=3; b14c=5;
+
+if (a14a>2 && a14a<11 || b14a>=6 && b14a<14){
+    console.log(v)
+}
+else{
+    console.log (n)
+}
+if (a14b>2 && a14b<11 || b14b>=6 && b14b<14){
+    console.log(v)
+}
+else{
+    console.log (n)
+}
+if (a14c>2 && a14c<11 || b14c>=6 && b14c<14){
+    console.log(v)
+}
+else{
+    console.log (n)
+}
+
+
+console.log('\n');
+
+console.log("Завдання 15")
+
+const num=3
+
+switch(num){
+    case 1:
+ console.log("зима")
+ break
+    case 2:
+ console.log("весна")
+ break  
+    case 3:
+console.log("літо")
+ break
+    case 4:
+console.log("осінь")
+ break
+}
